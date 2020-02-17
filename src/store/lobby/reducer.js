88705700c626faa86aacc603d6d4359fa65af84d
learnerdilip <diliptalks@gmail.com>
2020-02-17 17:48:21 +0100

    all working @day end

--- src/store/lobby/reducer.js
+++ src/store/lobby/reducer.js
@@ -1,11 +1,13 @@
-const initialState = [];
+const initialState = null;
 
 export default function roomReducer(state = initialState, action) {
   console.log("action", action);
 
   switch (action.type) {
     case "LOBBY_CREATED": {
-      return [...state, action.payload];
+      console.log("ACTION>PAYLOAD", action.payload);
+      const newState = action.payload;
+      return newState;
     }
     case "USER_LOGOUT":
       return { ...initialState };
